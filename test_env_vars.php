49de9a3ecd5a02cbade6aa6d24b49e9d56809ccd
Alex Ao Shen <ao.shen@gmail.com>
2021-05-28 19:43:35 -0400

    sdf

--- test_env_vars.php
+++ test_env_vars.php
@@ -4,8 +4,13 @@
 
 <?php
 
+echo 'display_errors = ' . ini_get('display_errors') . "\n";
+echo 'register_globals = ' . ini_get('register_globals') . "\n";
+echo 'post_max_size = ' . ini_get('post_max_size') . "\n";
+echo 'post_max_size+1 = ' . (ini_get('post_max_size')+1) . "\n";
+echo 'post_max_size in bytes = ' . return_bytes(ini_get('post_max_size'));
 
-echo "here ->";
+echo "<br>here ->";
 
 $gro_env = ini_get("grotrack.env");
 echo $gro_env;

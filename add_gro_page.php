540abd82182ac3b9b08d6a85d6d945385a861cd4
Tomer Zibman <tomerzibman@Tomers-MacBook-Air.local>
2021-05-28 16:59:33 -0400

    dempster bagles

--- add_gro_page.php
+++ add_gro_page.php
@@ -5,7 +5,17 @@
 <?php
 
 echo 'Hi this is add gro page';
-
+$conn = new mysqli("localhost:8889", "groperson", "gropassword", "groceries");
+$query = "SELECT gro_name, gro_exp_date FROM your_groceries;";
+$results = $conn -> query($query);
+echo "<table border='1'>";
+while($row = mysqli_fetch_assoc($results)) {
+    echo'<tr>';
+    echo "<td>" . $row['gro_name'] . "</td>";
+    echo "<td>" . $row['gro_exp_date'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
 ?>  
 
 </body>
--- create_gro_database.sql
+++ create_gro_database.sql
@@ -1,10 +1,11 @@
 CREATE DATABASE groceries;
-GRANT ALL ON importDB.* TO 'groperson'@'localhost';
+GRANT ALL ON groceries.* TO 'groperson'@'localhost';
 
 USE groceries;
 
 CREATE TABLE your_groceries(
-    gro_id INT,
+    gro_id INT AUTO_INCREMENT,
+    gro_barcode VARCHAR(50),
     gro_name VARCHAR(20),
     gro_amount INT,
     gro_exp_date DATE,

e7f127642bbd168b35c43cae9f06d436a6696f97
Alex Ao Shen <ao.shen@gmail.com>
2021-05-28 19:29:39 -0400

    sdf

--- test_env_vars.php
+++ test_env_vars.php
@@ -10,7 +10,14 @@ echo "here ->";
 $gro_env = $_ENV["GRO_ENV"];
 echo $gro_env;
 
-echo "<- after here";
+echo "<- after here<br>";
+
+var_dump($_ENV);
+echo "<br>";
+var_dump($_SERVER);
+echo "<br>";
+
+phpinfo();
 
 ?>  
 

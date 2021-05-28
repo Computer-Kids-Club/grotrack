2f0ffdd30bd2b6ef34c912e35c91ca87647eacec
Daniel Kulakouski <dan.kulakouski@gmail.com>
2021-05-28 16:22:01 -0400

    barcode page displays more info

--- barcode.php
+++ barcode.php
@@ -3,29 +3,37 @@
 <body>
 
 <?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.<br>";
-
 $examplebarcode = "737628064502";
 $handsani = "067153948160";
 $granola = "060383046019";
+$tuna = "8004030044005";
 
-$xml = json_decode(file_get_contents("https://world.openfoodfacts.org/api/v0/product/$examplebarcode.json"));
+$xml = json_decode(file_get_contents("https://world.openfoodfacts.org/api/v0/product/$tuna.json"));
 // var_dump($xml);
-// echo json_encode($xml, JSON_PRETTY_PRINT);
 
-echo "<br><br><br>";
-echo $xml->product->_id;
-echo "<br>";
-echo json_encode($xml->product->_keywords);
-echo "<br>";
+$name = ucwords($xml->product->product_name);
+echo "<h2>$name</h2>";
 $image = urldecode($xml->product->image_front_small_url);
-// $image = "https://static.openfoodfacts.org/images/products/073/762/806/4502/front_en.6.200.jpg";
-echo $image;
 $imageData = base64_encode(file_get_contents($image));
 echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
+echo "<br>";
+$keywords = json_encode($xml->product->_keywords);
+// echo $keywords;
+$nutrients = json_encode($xml->product->nutriments);
+echo "Nutritional Facts: $nutrients";
+
+$ingredients = json_encode($xml->product->ingredients_text_en);
+echo "<br><br>";
+$ingredients = str_replace(['"',"'"], "", $ingredients);
+echo "Ingredients: $ingredients";
+
+
+
+// echo "<br><br><br>";
+
+// echo "<br>";
+// echo json_encode($xml->product->_keywords);
+// echo "<br>";
 
 // echo $xml;
 

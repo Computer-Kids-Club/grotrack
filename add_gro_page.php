5136581d78d503d2a9212044c10c5b8d008e72e0
Tomer Zibman <tomerzibman@Tomers-MacBook-Air.local>
2021-05-29 00:38:08 -0400

    fix database and such

--- add_gro_page.php
+++ add_gro_page.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
-<h1 class='has-background-success-light is-size-1 has-text-left ml-7'>Grotrack</h1>
+<h1 class='has-background-success-light is-size-1'>Grotrack</h1>
 <!-- <style type="text/css">
         table.mytable {
             border: 1px solid Green;
@@ -40,7 +40,9 @@ if (!is_null($_SERVER["SERVER_NAME"]) && $_SERVER["SERVER_NAME"] ===  "www.grotr
     $host = "localhost:3306";
 }
 $conn = new mysqli($host, "groperson", "gropassword", "groceries");
-$query = "SELECT gro_name, gro_exp_date FROM your_groceries;";
+//$uuid = ???
+//$query = "SELECT groceries.name, groceries.exp_date FROM groceries INNER JOIN users ON groceries.user_id = users.id WHERE users.uuid = '".$uuid';";
+$query = "SELECT name, exp_date FROM groceries;";
 $results = $conn -> query($query);
 echo "<table class = 'has-text-centered has-background-success-light mytable'>";
 echo "<tr>";
@@ -49,8 +51,8 @@ echo "<td>" . '<b class = "is-size-3 mx-3"> Expiration Date </b>' . "</td>";
 echo "</tr>";
 while($row = mysqli_fetch_assoc($results)) {
     echo"<tr>";
-    echo "<td>" . $row['gro_name'] . "</td>";
-    echo "<td>" . $row['gro_exp_date'] . "</td>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "<td>" . $row['exp_date'] . "</td>";
     echo "</tr>";
 }
 echo "</table>";

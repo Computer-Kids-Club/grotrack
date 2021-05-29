6a9b088a791ad6015c2402f9c3fa383a9f246146
Daniel Kulakouski <dan.kulakouski@gmail.com>
2021-05-29 17:18:04 -0400

    barcode page pretty much done

--- barcode.php
+++ barcode.php
@@ -1,98 +1,44 @@
 <!DOCTYPE html>
 <html>
-
 <title>GroTrack</title>
-
+<head>
+    <link rel="stylesheet" href="coolertable.css">
+</head>
 <body>
 
 <h1>GroTrack</h1>
 
 <!-- // $examplebarcode = "737628064502";
-// $handsani = "067153948160";
 // $granola = "060383046019";
-// $tuna = "8004030044005"; -->
+// $tuna = "8004030044005";
+$cheerios =  -->
 
 <!-- <a class="button" id="startButton">Click To Start Scanning Barcode</a> -->
 <?php
 if (!isset($_GET['bc']) && !isset($_POST['submitBarcode'])) {
 ?>
 
-<button type="button" id="startButton">Scan Barcode</button>
-<button type="button" id="resetButton">Reset</button>
-<br>
+    <button type="button" id="startButton">Scan Barcode</button>
+    <button type="button" id="resetButton">Reset</button>
+    <br>
 
-<form action="barcode.php" method="post">
-Barcode: <input type="text" name="barcode"><br>
-<input type="submit" name="submitBarcode" value="Enter">
-</form>
+    <form action="barcode.php" method="post">
+    Barcode: <input type="text" name="barcode"><br>
+    <input type="submit" name="submitBarcode" value="Enter">
+    </form>
 
-<!-- <a class="button" id="resetButton">Reset Barcode</a> -->
+    <div>
+        <canvas id="canvas" width="600" height="400" style="border: 1px solid gray"></canvas>
+    </div>
 
-<div>
-    <canvas id="canvas" width="600" height="400" style="border: 1px solid gray"></canvas>
-</div>
+    <div id="sourceSelectPanel" style="display:none">
+        <label for="sourceSelect">Change video source:</label>
+        <select id="sourceSelect" style="max-width:400px">
+        </select>
+    </div>
 
-<div id="sourceSelectPanel" style="display:none">
-    <label for="sourceSelect">Change video source:</label>
-    <select id="sourceSelect" style="max-width:400px">
-    </select>
-</div>
-
-<label>Result:</label>
-<pre><code id="result"></code></pre>
-<!-- <input type="submit" name="enterBarcode" value="Enter"> -->
-
-<!-- <script type="text/javascript" src="https://unpkg.com/@zxing/library@latest"></script>
-    <script type="text/javascript">
-        window.addEventListener('load', function () {
-            let selectedDeviceId;
-            const codeReader = new ZXing.BrowserBarcodeReader()
-            console.log('ZXing code reader initialized')
-            codeReader.getVideoInputDevices()
-                .then((videoInputDevices) => {
-                    const sourceSelect = document.getElementById('sourceSelect')
-                    selectedDeviceId = videoInputDevices[0].deviceId
-                    if (videoInputDevices.length > 1) {
-                        videoInputDevices.forEach((element) => {
-                            const sourceOption = document.createElement('option')
-                            sourceOption.text = element.label
-                            sourceOption.value = element.deviceId
-                            sourceSelect.appendChild(sourceOption)
-                        })
-
-                        sourceSelect.onchange = () => {
-                            selectedDeviceId = sourceSelect.value;
-                        }
-
-                        const sourceSelectPanel = document.getElementById('sourceSelectPanel')
-                        sourceSelectPanel.style.display = 'block'
-                    }
-
-                    document.getElementById('startButton').addEventListener('click', () => {
-                        codeReader.decodeOnceFromVideoDevice(selectedDeviceId, 'video').then((result) => {
-                            console.log(result)
-                            document.getElementById('result').textContent = result.text
-                            window.location.href="barcode.php?bc="+result.text
-                        }).catch((err) => {
-                            console.error(err)
-                            document.getElementById('result').textContent = err
-                        })
-                        console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
-                    })
-
-                    document.getElementById('resetButton').addEventListener('click', () => {
-                        document.getElementById('result').textContent = '';
-                        window.location.href="barcode.php"
-                        codeReader.reset();
-                        console.log('Reset.')
-                    })
-
-                })
-                .catch((err) => {
-                    console.error(err)
-                })
-        })
-    </script> -->
+    <label>Result:</label>
+    <pre><code id="result"></code></pre>
 
     <hr>
         <select></select>
@@ -130,88 +76,191 @@ Barcode: <input type="text" name="barcode"><br>
     // echo "hi";
     // $barcode = $_POST["barcode"];
     // $barcode = "8004030044005";
-    if(isset($_GET['bc'])) {
-        $barcode = $_GET["bc"];
-    } else {
-        $barcode = $_POST['barcode'];
+    if(isset($_GET['bc'])) $barcode = $_GET["bc"];
+    else $barcode = $_POST['barcode'];
         // echo $barcode;
-    }
 
     // echo $barcode;
 
     // echo "$barcode       $barcode2";
     // $barcode = <code id="result"></code>;
-    $xml = json_decode(file_get_contents("https://world.openfoodfacts.org/api/v0/product/$barcode.json"));
+    $xml = file_get_contents("https://world.openfoodfacts.org/api/v0/product/$barcode.json");
+    $xml = json_decode($xml);
     // var_dump($xml);
 
     $name = ucwords($xml->product->product_name);
     echo "<h2>$name</h2>";
     $image = urldecode($xml->product->image_front_small_url);
     $imageData = base64_encode(file_get_contents($image));
-    echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
-    echo "<br>";
-    $keywords = json_encode($xml->product->_keywords);
-    // echo $keywords;
-    $nutrients = json_encode($xml->product->nutriments);
-    echo "Nutritional Facts: $nutrients";
+    ?>
+    <div class="center">
+    <?php
+    echo '<p style="float: left;"><img src="data:image/jpeg;base64,'.$imageData.'" width=300vw></p>';
+    // echo "<br>";
 
-    $ingredients = json_encode($xml->product->ingredients_text_en);
+    // for searching later
+    // $keywords = json_encode($xml->product->_keywords);
+
+    $nutrients = $xml->product->nutriments;
+    // echo $nutrients[0];
+    // echo "<br><br>";
+    ?>
+    <section class="performance-facts">
+  <header class="performance-facts__header">
+    <h1 class="performance-facts__title">Nutrition Facts</h1>
+  </header>
+  <table class="performance-facts__table">
+    <!-- <thead> -->
+      <!-- <tr>
+        <th colspan="3" class="small-info">
+          Amount Per Serving
+        </th>
+      </tr> -->
+    <!-- </thead> -->
+    <tbody>
+      <!-- <tr> -->
+        <!-- <th colspan="2"> -->
+          <b>Calories </b>
+            <?php
+            echo "<b>", $nutrients->{'energy-kcal'}, "<b>";
+            unset($nutrients->{'energy-kcal'});
+            ?>
+      <tr class="thick-row">
+        <td colspan="3" class="small-info">
+        </td>
+      </tr>
+                                    <!-- <tr>
+                                        <th colspan="2">
+                                        <b>Total Fat</b>
+                                        14g
+                                        </th>
+                                    </tr> -->
+      <!-- <tr> -->
+        <!-- <td class="blank-cell">
+        </td>
+        <th>
+          Saturated Fat
+          9g
+        </th>
+        <td>
+          <b>22%</b>
+        </td>
+      </tr>
+      <tr>
+        <td class="blank-cell">
+        </td>
+        <th>
+          Trans Fat
+          0g
+        </th>
+        <td>
+        </td> -->
+      <!-- </tr> -->
+                                                    <!-- <tr>
+                                                        <th colspan="2">
+                                                        <b>Cholesterol</b>
+                                                        55mg
+                                                        </th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th colspan="2">
+                                                        <b>Sodium</b>
+                                                        40mg
+                                                        </th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th colspan="2">
+                                                        <b>Total Carbohydrate</b>
+                                                        17g
+                                                        </th>
+                                                    </tr> -->
+      <!-- <tr> -->
+        <!-- <td class="blank-cell">
+        </td>
+        <th>
+          Dietary Fiber
+          1g
+        </th>
+        <td>
+          <b>4%</b>
+        </td>
+      </tr>
+      <tr>
+        <td class="blank-cell">
+        </td>
+        <th>
+          Sugars
+          14g
+        </th>
+        <td>
+        </td> -->
+      <!-- </tr> -->
+      <!-- <tr class="thick-end"> -->
+                                                    <!-- <th colspan="2">
+                                                    <b>Protein</b>
+                                                    3g
+                                                    </th> -->
+    <!-- </tbody>
+  </table> -->
+
+<!-- </section> -->
+    <?php
+    $prevKey = "";
+    $prevValue = 0;
+    $unit = "";
+    foreach($nutrients as $key => $value) {
+        if(strpos($key, "nova") === false && strpos($key, "_100g") === false && (strpos($key, "_value") !== false || strpos($key, "_unit") !== false) && $key != "energy-kj" &&
+        strpos($key, "nutrition") === false && strpos($key, "_serving") === false && strpos($key, "energy") === false) {
+            // if(strpos($key, "_unit") !== false) {
+                // if($prev != "energy-kcal")echo $value;
+            // } else {
+                // echo "<br>";
+                // if($key == "energy-kcal") echo "Calories ", $value;
+                // if(round($value, 1) != 0) {
+                if(strpos($prevKey, "_unit") !== false) {
+                    $unit = $prevValue;
+                } else {
+                    $unit = "";
+                }
+
+                if(strpos($key, "_value") !== false) {
+                ?>
+
+                <tr>
+                    <th colspan="2">
+                        <b><?php echo ucwords(str_replace("_value", "", $key)) ?></b>
+                        <?php echo round($value, 1), $unit; ?>
+                    </th>
+                </tr>
+                <?php
+                }
+                
+                // echo ucwords($key), " ", $value;
+                $prevKey = $key;
+                $prevValue = $value;
+            // }
+        }
+    }
+
+    ?>
+            </tbody>
+        </table>
+    </section>
+</div>
+    <?php
+
     echo "<br><br>";
+    $ingredients = json_encode($xml->product->ingredients_text_en);
     $ingredients = str_replace(['"',"'"], "", $ingredients);
-    echo "Ingredients: $ingredients";
+    if(!is_null($ingredients)) echo "Ingredients: $ingredients";
+    echo "<br>";
 }
 
-
-
-// $examplebarcode = "737628064502";
-// $handsani = "067153948160";
-// $granola = "060383046019";
-// $tuna = "8004030044005";
-
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // if (isset($_POST['submitBarcode'])) {
-// if (isset($_GET['bc'])) {
-//     echo "hi";
-//     // $barcode = $_POST["barcode"];
-//     // $barcode = "8004030044005";
-//     $barcode = $_GET["bc"];
-//     // $barcode = <code id="result"></code>;
-//     $xml = json_decode(file_get_contents("https://world.openfoodfacts.org/api/v0/product/$barcode.json"));
-//     // var_dump($xml);
-
-//     $name = ucwords($xml->product->product_name);
-//     echo "<h2>$name</h2>";
-//     $image = urldecode($xml->product->image_front_small_url);
-//     $imageData = base64_encode(file_get_contents($image));
-//     echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
-//     echo "<br>";
-//     $keywords = json_encode($xml->product->_keywords);
-//     // echo $keywords;
-//     $nutrients = json_encode($xml->product->nutriments);
-//     echo "Nutritional Facts: $nutrients";
-
-//     $ingredients = json_encode($xml->product->ingredients_text_en);
-//     echo "<br><br>";
-//     $ingredients = str_replace(['"',"'"], "", $ingredients);
-//     echo "Ingredients: $ingredients";
-// } else {
-    // echo "2";
-    // Assume btnSubmit
-// }
-// }
-
-
-
-
-// echo "<br><br><br>";
-
-// echo "<br>";
-// echo json_encode($xml->product->_keywords);
-// echo "<br>";
-
-// echo $xml;
-
-?> 
+?>
+<button>Add To Groceries</button>
+<form action="https://grotrack.co/barcode.php">
+    <input type="submit" value="Go Back" />
+</form>
 
 </body>
 </html>

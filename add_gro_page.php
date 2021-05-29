1d01bd7397524eecd7a6b6819f03547f694468dc
Tomer Zibman <tomerzibman@Tomers-MacBook-Air.local>
2021-05-29 19:55:15 -0400

    updated messages

--- add_gro_page.php
+++ add_gro_page.php
@@ -44,6 +44,7 @@ $uuid = $session_uuid;
 $query = "SELECT groceries.id, groceries.name, groceries.amount, groceries.exp_date FROM groceries INNER JOIN users ON groceries.user_id = users.id WHERE users.uuid = '".$uuid."';";
 $results = $conn -> query($query);
 $almost_expired = array();
+$expired = array();
 date_default_timezone_set('America/Toronto');
 $date = date('Y-m-d H:i:s');
 echo "<table class = 'has-text-centered has-background-success-light mytable'>";
@@ -54,7 +55,7 @@ echo "<td>" . '<div class = "is-size-3 mx-3"> Expiration Date </div>' . "</td>";
 echo "<td>" . '<div class = "is-size-3"> Consume <div>' . "</td>";
 echo "</tr>";
 while($row = mysqli_fetch_assoc($results)) {
-    $date_diff = (strtotime($row['exp_date']) - strtotime($date))/86400;
+    $date_diff = ((strtotime($row['exp_date']) - strtotime($date)))/86400;
     echo"<tr>";
     echo "<td>" . $row['name'] . "</td>";
     echo "<td>" . $row['amount'] . "</td>";
@@ -69,17 +70,37 @@ while($row = mysqli_fetch_assoc($results)) {
     }
     echo "<td>" . '<form action="update_amount.php" method="get"><button name="id" value="'.$row['id'].'" class="button is-outlined is-small is-primary mb-1"> Consume </button></form>' . "</td>";
     echo "</tr>";
-    if(((strtotime($row['exp_date']) - strtotime($date))/86400) < 1){
+    if($date_diff <= 1 && $date_diff > 0){
         array_push($almost_expired, $row['exp_date']);
+    }
+    elseif($date_diff <= 0){
+        array_push($expired, $row['exp_date']);
     }
 }
 echo "</table>";
 $num_expiring = sizeof($almost_expired);
+$num_expired = sizeof($expired);
 if($num_expiring == 1 && !$_GET['no_msg']){
-    echo "<script type='text/javascript'>alert('You have $num_expiring grocery product expiring!');</script>";
+    if($num_expired == 1){
+        echo "<script type='text/javascript'>alert('You have $num_expiring grocery product expiring! You have $num_expired expired grocery product!');</script>";
+    }
+    else if($num_expired > 1){
+        echo "<script type='text/javascript'>alert('You have $num_expiring grocery product expiring! You have $num_expired expired grocery products!');</script>";
+    }
+    else{
+        echo "<script type='text/javascript'>alert('You have $num_expiring grocery product expiring!');</script>";
+    }
 }
 else if($num_expiring > 1 && !$_GET['no_msg']){
-    echo "<script type='text/javascript'>alert('You have $num_expiring grocery products expiring!');</script>";
+    if($num_expired == 1){
+        echo "<script type='text/javascript'>alert('You have $num_expiring grocery products expiring! You have $num_expired expired grocer prodcut!');</script>";
+    }
+    else if($num_expired > 1){
+        echo "<script type='text/javascript'>alert('You have $num_expiring grocery products expiring! You have $num_expired expired grocer prodcuts!');</script>";
+    }
+    else{
+        echo "<script type='text/javascript'>alert('You have $num_expiring grocery products expiring!');</script>";
+    }
 }
 ?>
 <br/>
